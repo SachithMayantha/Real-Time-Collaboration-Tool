3015d73c0b9b19a55845c56c135dd0c31f5a08b1
SachithMayantha <sachithfernando599@gmail.com>
2024-07-06 14:03:37 +1000

    initial

---
+++ Backend/SyncSpace/SyncSpace/src/test/java/com/idea/syncspace/SyncSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea.syncspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
